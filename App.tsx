635cffeee94fc775eebffa7792a51b3cab6f0b34
dferreira11 <144393635+dferreira11@users.noreply.github.com>
2024-01-21 22:12:02 -0500

    App.tsx

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import "./App.css";
+import "./App.css";
+import Snake from "./Snake.ts";
+import display from "./display";
+import { useEffect } from "react";
+
+export default function App() {
+  // Add Snake Tests with display below
+  useEffect(() => {
+    // Include your display statements to test below
+    document.getElementById("output")!.innerText = "OUTPUT:\n";
+    display("hey");
+    const snake = new Snake();
+    snake.move();
+    snake.turn();
+    const worldModel = new WorldModel(snake);
+  }, []);
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <pre id="output">
+        OUTPUT: <br />
+      </pre>
+    </div>
+  );
+}
