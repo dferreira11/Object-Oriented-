98af2a33187c5da8c387e1a115e2026923bffcaf
dferreira11 <144393635+dferreira11@users.noreply.github.com>
2024-02-16 14:26:04 -0500

    Update Snake.ts

--- Snake.ts
+++ Snake.ts
@@ -6,12 +6,10 @@ import Point from "./Point";
 class Snake {
   private currentPosition: Point;
   private currentDirection: number;
-
   constructor() {
     this.currentPosition = new Point(0, 0);
     this.currentDirection = 1;
   }
-
   turnLeft() {
     this.currentDirection = (this.currentDirection + 3) % 4;
   }
@@ -19,10 +17,36 @@ class Snake {
   turnRight() {
     this.currentDirection = (this.currentDirection + 1) % 4;
   }
-
-  move(distance: number) {}
-
-  get direction(): number {
+  move(distance: number) {
+    if (this.currentDirection === 0) {
+      this.currentPosition = new Point(
+        this.currentPosition.x,
+        this.currentPosition.y - distance,
+      );
+    } else if (this.currentDirection === 1) {
+      this.currentPosition = new Point(
+        this.currentPosition.x + distance,
+        this.currentPosition.y,
+      );
+    } else if (this.currentDirection === 2) {
+      this.currentPosition = new Point(
+        this.currentPosition.x,
+        this.currentPosition.y + distance,
+      );
+    } else if (this.currentDirection === 3) {
+      this.currentPosition = new Point(
+        this.currentPosition.x - distance,
+        this.currentPosition.y,
+      );
+    }
+  }
+  turn() {
+    this.currentDirection *= -1;
+  }
+  get currentposition(): Point {
+    return this.currentPosition;
+  }
+  get currentdirection(): number {
     return this.currentDirection;
   }
 }
