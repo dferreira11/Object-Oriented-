6f095616d4ecad66da8e4555e1926938fb7c4ae4
dferreira11 <144393635+dferreira11@users.noreply.github.com>
2024-03-24 22:11:24 -0400

    Update GameController.ts

--- GameController.ts
+++ GameController.ts
@@ -1,1 +1,38 @@
+class GameController {
+    private world: WorldModel;
+    private player1: Player;
+    private player2: Player;
 
+    constructor(world: WorldModel) {
+        this.world = world;
+    }
+
+    setPlayer1(player: Player): void {
+        this.player1 = player;
+    }
+
+    setPlayer2(player: Player): void {
+        this.player2 = player;
+    }
+
+    run(): void {
+        let lastTime = 0;
+
+        const updateFrame = () => {
+            this.player1.makeTurn();
+            this.player2.makeTurn();
+
+            const currentTime = Date.now();
+            const elapsedTime = currentTime - lastTime;
+
+            if (elapsedTime > 250) {
+                this.world.update(1);
+                lastTime = currentTime - (elapsedTime % 250);
+            }
+
+            requestAnimationFrame(updateFrame);
+        };
+
+        updateFrame();
+    }
+}
