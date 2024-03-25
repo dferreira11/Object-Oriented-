90894897a317ed1241e823078906b34a89e17604
dferreira11 <144393635+dferreira11@users.noreply.github.com>
2024-03-24 22:10:55 -0400

    Update HumanPlayer.ts

--- HumanPlayer.ts
+++ HumanPlayer.ts
@@ -1,1 +1,18 @@
+class HumanPlayer extends Player {
+    private inputHandler: IInputHandler;
 
+    constructor(snakeController: SnakeController, inputHandler: IInputHandler) {
+        super(snakeController);
+        this.inputHandler = inputHandler;
+    }
+
+    makeTurn(): void {
+        if (this.inputHandler.madeLeftMove()) {
+            this.sc.turnSnakeLeft();
+            this.inputHandler.resetLeftMove();
+        } else if (this.inputHandler.madeRightMove()) {
+            this.sc.turnSnakeRight();
+            this.inputHandler.resetRightMove();
+        }
+    }
+}
