d5a45c698f791afc8900ce6fccd7f5d174476f06
dferreira11 <144393635+dferreira11@users.noreply.github.com>
2024-05-22 21:02:47 -0400

    Update Player.ts

--- Player.ts
+++ Player.ts
@@ -1,11 +1,18 @@
 import SnakeController from "./SnakeController";
-abstract class Player {
-  protected sc: SnakeController;
 
-  constructor(sc: SnakeController) {
-    this.sc = sc;
+//Abstract class that represents a player in the Snake game.The subclasses of Player will define the specific behaviors that a player have
+
+abstract class Player {
+  protected scs: SnakeController[];
+
+  //Constructs a new Player instance. @param snakeController - The SnakeController associated with the player.
+   
+  constructor(snakeControllers: SnakeController[]) {
+    this.scs = snakeControllers;
   }
 
+  //Abstract method that defines the behavior of the player for making a turn in the game.The subclasses have to implement this method in order to specify the player's logic for making a turn.
   abstract makeTurn(): void;
 }
+
 export default Player;
