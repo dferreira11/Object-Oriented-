33cac6f1b8ae06888ce2e2b07f7427b2f43a9c80
dferreira11 <144393635+dferreira11@users.noreply.github.com>
2024-05-22 21:04:32 -0400

    Update Point.ts

--- Point.ts
+++ Point.ts
@@ -1,18 +1,30 @@
-class Point {
-  private readonly xcoord: number;
-  private readonly ycoord: number;
+export default class Point {
+  private x_coordinate__: number;
+  private y_coordinate__: number; 
 
   constructor(x: number, y: number) {
-    this.xcoord = x;
-    this.ycoord = y;
+    this.x_coordinate__ = x;
+    this.y_coordinate__ = y;
+  }
+  public equals(other: Point): boolean {
+    return this.x === other.x && this.y === other.y;
   }
 
-  get x(): number {
-    return this.xcoord;
+  public get x(): number {
+    //Gets the x coordinate of the point.@returns the x coordinate
+     
+    return this.x_coordinate__;
   }
 
-  get y(): number {
-    return this.ycoord;
+  public get y(): number {
+    //Gets the y coordinate of the point.@returns the y coordinate
+     
+    return this.y_coordinate__;
+  }
+
+  public get Position(): Point {
+    //Gets the position of the point.@returns a new Point object with the same coordinates
+     
+    return new Point(this.x, this.y);
   }
 }
-export default Point;
